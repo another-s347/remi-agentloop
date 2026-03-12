eddec985401037df64ce736a7477dfe48e40cd26
another-s347 <s347419313@gmail.com>
2026-03-13 00:26:26 +0800

    fix: add user-state-json to loop-input-start WIT record and guest deserialization

--- remi-agentloop-guest/src/lib.rs
+++ remi-agentloop-guest/src/lib.rs
@@ -570,6 +570,8 @@ macro_rules! export_agent {
                         max_tokens: s.max_tokens,
                         metadata: s.metadata_json
                             .and_then(|j| $crate::_serde_json::from_str(&j).ok()),
+                        user_state: s.user_state_json
+                            .and_then(|j| $crate::_serde_json::from_str(&j).ok()),
                     },
                     wit::LoopInput::Resume(r) => $crate::LoopInput::Resume {
                         state: wit_state_to_rust(r.state),
